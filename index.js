587eaeb97da63de14cc5892eea3baf8db3e5d18e
Sidraaslam451 <aslamsidra876@gmail.com>
2025-08-10 10:13:15 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// ASSIGNMENT OF JAVA SCRIPT
+// QUESTION 01
+alert("JAVASCRIPT ALERT \n \n  HELLO WORLD ! I'm here to help you with your javascript alert box");
+// QUESTION 02
+alert("JAVASCRIPT ALERT \n \n Error ! please enter a valid password");
+// QUESTION 03
+alert("JAVASCRIPT ALERT \n \n Welcome to JS land \n Happy Coding !");
+// QUESTION 04
+alert("JAVASCRIPT ALERT \n \n Welcome to JS land");
+
+alert("Happy Coding ! \n \n ");
+// QUESTION 05
+alert("JAVASCRIPT ALERT \n \n Hello .. I can run JS from my browser's console")
